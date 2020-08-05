8f4062bd72489b213ba946582ad270a331390e0e
smallZydrius <zydrunas.pavinksnis@gmailcom>
2020-08-05 21:39:04 +0300

    two tables. empl OR proj

--- index.php
+++ index.php
@@ -3,9 +3,31 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Darbuotojai projektai</title>
+    <title>Employees & Projects</title>
 </head>
 <body>
+
+    <table border="1"><tr><th>
+        <form action="index.php" method="POST">
+            <input type="hidden"  name="empl" value="employees">
+            <a href="#" onclick="this.parentNode.submit();">Employees</a>
+        </form>
+    </th><th>
+
+        <form action="index.php" method="POST">
+            <input type="hidden"  name="proj" value="actions">
+            <a href="#" onclick="this.parentNode.submit();">Projects</a>
+        </form>
+    </th></tr>
+    </table>
+
+
+    <br><br>
+
+
+
+     
+
     <?php
     $servername = "localhost";
     $username = "root";
@@ -19,23 +41,43 @@
         die("Connection failed: " . mysqli_connect_error());
     }
 
-    $sql = "SELECT id, name, surname FROM employees";
-    $result = mysqli_query($conn, $sql);
-
-    if (mysqli_num_rows($result) > 0) {
-       echo '<table border="1">';
-       echo '<tr><th>id</th><th>name</th><th>surname</th></tr>';
-       while($row = mysqli_fetch_assoc($result)) {
-
-            echo '<tr><td>'.$row["id"].'</td><td>'.$row["name"].'</td><td>'.$row["surname"].'</td</tr>';
-            
-            
-        }
-
-        echo '</table>';
-    } else {
-        echo "0 results";
+    if (isset($_POST['empl'])) {
+        $tableName = $_POST['empl'];
+                    
+        $sql = "SELECT id, name, surname FROM $tableName ";
+        $result = mysqli_query($conn, $sql);
+    
+        if (mysqli_num_rows($result) > 0) {
+            echo '<table border="1">';
+            echo '<tr><th>employee id</th><th>name</th><th>surname</th><th>project</th></tr>';
+            while($row = mysqli_fetch_assoc($result)) {
+                echo '<tr><td>'.$row["id"].'</td><td>'.$row["name"].'</td><td>'.$row["surname"].'</td><td></td></tr>';                
+            }
+            echo '</table>';
+        } else {
+            echo "0 results";
+        }    
     }
+
+    if (isset($_POST['proj'])) {
+        $tableName = $_POST['proj'];
+                    
+        $sql = "SELECT id, projectname FROM $tableName ";
+        $result = mysqli_query($conn, $sql);
+    
+        if (mysqli_num_rows($result) > 0) {
+            echo '<table border="1">';
+            echo '<tr><th>project id</th><th>project name</th><th>responsible empoyee(s)</th></tr>';
+            while($row = mysqli_fetch_assoc($result)) {
+                echo '<tr><td>'.$row["id"].'</td><td>'.$row["projectname"].'</td><td></td></tr>';                
+            }
+            echo '</table>';
+        } else {
+            echo "0 results";
+        }    
+    }
+
+    
 
     mysqli_close($conn);
     ?>
